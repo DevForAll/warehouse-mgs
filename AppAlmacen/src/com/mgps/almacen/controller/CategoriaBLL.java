5bd503066fe1e2eb5240b97f3ebf2bf3e110c4c7
Jhon Kennedy <jhonlt40@gmail.com>
2020-02-26 06:13:35 -0500

    servicio de Actualizar Categoria

--- AppAlmacen/src/com/mgps/almacen/controller/CategoriaBLL.java
+++ AppAlmacen/src/com/mgps/almacen/controller/CategoriaBLL.java
@@ -1,30 +1,22 @@
 package com.mgps.almacen.controller;
 
-
 import com.mgps.almacen.dao.CategoriaDAO;
 import com.mgps.almacen.entity.CategoriaTO;
 
-
-
-
-
 public class CategoriaBLL {
 
-	
-	
 	CategoriaDAO dao;
 
-	  public CategoriaBLL() {
-	    dao = new CategoriaDAO();
-	  }
+	public CategoriaBLL() {
+		dao = new CategoriaDAO();
+	}
 
+	//	METODOS DEL NEGOCIO
+    public int CategoriaAdicionar(CategoriaTO x) throws Exception {
+        return dao.create(x);
+    }
 
-	    public int CategoriaAdicionar(CategoriaTO x) throws Exception {
-	        return dao.create(x);
-	    }
-
-	  
-	  
-	  
-	  
+	public int CategoriaActualizar(CategoriaTO x) throws Exception {
+		return dao.update(x);
+	}
 }
